734b1a3efe290e373a727651725c3ed218bf646d
Wlisses Silva <104795256+Wlisses-Silva@users.noreply.github.com>
2022-07-01 16:21:39 -0300

    Criando uma Vnet

--- main.tf
+++ main.tf
@@ -17,3 +17,9 @@ resource "azurerm_resource_group" "rg" {
   tags     = var.tags
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  location            = var.location
+  resource_group_name = var.resource_group
+  address_space       = ["10.0.0.0/16"]
+}
